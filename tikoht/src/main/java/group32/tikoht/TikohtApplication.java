60b7f9f3c7987fb5c838bc76f74fae27db39dc3e
Joonas Ilvonen <joonas.ilvonen@outlook.com>
2020-04-20 16:02:06 +0300

    Lopullisen JAVA Projektin alustus.

---
+++ tikoht/src/main/java/group32/tikoht/TikohtApplication.java
@@ -0,0 +1,13 @@
+package group32.tikoht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikohtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikohtApplication.class, args);
+	}
+
+}
---
+++ tikoht/src/test/java/group32/tikoht/TikohtApplicationTests.java
@@ -0,0 +1,13 @@
+package group32.tikoht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikohtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
