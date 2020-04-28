684f3a33282510d15710eef021c72d6f96723efe
joonasil <joonas.ilvonen@outlook.com>
2020-04-28 18:53:17 +0300

    Dates again.

--- tikoht/src/main/java/group32/tikoht/model/Tyosuoritus.java
+++ tikoht/src/main/java/group32/tikoht/model/Tyosuoritus.java
@@ -1,27 +1,20 @@
 package group32.tikoht.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.time.LocalDate;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class Tyosuoritus {
 
     private final Integer suoritusid;
     private final Integer sopimusid;
-    // @NotBlank
+    @NotNull
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    @JsonSerialize(using = LocalDateSerializer.class)
     private final LocalDate suorituspvm;
 
     public Tyosuoritus( @JsonProperty("suoritusid") Integer suoritusid,
